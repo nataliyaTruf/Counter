2deade5f203a50740a6555678a25f940b443a5c4
NataliyaMassol <135216859+nataliyaTruf@users.noreply.github.com>
2023-06-02 15:45:05 +0200

    Создала счетчик

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var countLabel: UILabel!
+    
+    @IBOutlet weak var plusButton: UIButton!
+    
+    @IBOutlet weak var minusButton: UIButton!
+    
+    @IBOutlet weak var resetButton: UIButton!
+    
+    @IBOutlet weak var historyOfChangesText: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        historyOfChangesText.isEditable = false
+        
         // Do any additional setup after loading the view.
     }
-
+    
+    var count = 0
+    func getCurrentDate() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        let dateString = formatter.string(from: Date())
+       return dateString
+    }
+    @IBAction private func plusButtonDidTap(_ sender: Any) {
+        let currentDate = getCurrentDate()
+        count += 1
+        countLabel.text = "\(count)"
+        historyOfChangesText.text += "\n \(currentDate) : значение изменено на +1"
+    }
+    @IBAction private func minusButtonDidTap(_ sender: Any) {
+        let currentDate = getCurrentDate()
+        if count > 0 {
+            count -= 1
+            historyOfChangesText.text += "\n \(currentDate): значение изменено на -1"
+        } else {
+            count = 0
+            historyOfChangesText.text += "\n \(currentDate): попытка уменьшить значение счётчика ниже 0"
+        }
+        countLabel.text = "\(count)"
+        
+    }
+    
+    @IBAction private func resetButtonDidTap(_ sender: Any) {
+        let currentDate = getCurrentDate()
+        countLabel.text = "\(0)"
+        count = 0
+        historyOfChangesText.text = "\n \(currentDate) значение сброшено"
+    }
 
 }
 
